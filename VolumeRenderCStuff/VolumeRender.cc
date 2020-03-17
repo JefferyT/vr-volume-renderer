90ad379bc54251504ed68e9d2616634149dea422
Jeffery Tian <jeff.f.t888@outlook.com>
2020-03-17 15:07:30 -0700

    working on the shader

---
+++ VolumeRenderCStuff/VolumeRender.cc
@@ -0,0 +1,22 @@
+/*
+ * Copyright ©2019 Jeffery Tian.  All rights reserved.
+ * Idk how to do this copyright stuff. Use it if you want.
+ */
+
+#include <stdio.h>
+#include <cstdlib>
+#include <string>
+#include "Volume.h"
+
+using std::string;
+
+int main(int argc, char** argv) {
+  Volume *vol;
+  if (argc == 2) {
+    vol = new Volume(argv[1], 512, 512, 406);
+  } else {
+    vol = new Volume("body.dat", 512, 512, 406);
+  }
+
+  vol->PrintSlice(255);
+}
--- VolumeRenderCStuff/read.cc
+++ VolumeRenderCStuff/read.cc
@@ -1,21 +1,10 @@
 #include <cstdlib>
 #include <cstdio>
 
-void SaveVolume(std::shared_ptr<CSliceStackVolume> i_pVolume) {
-  FILE *fp = fopen("body.dat", "wb");
-  int dimX = i_pVolume->m_volumeMetaData.GetXResolution(),
-      dimY = i_pVolume->m_volumeMetaData.GetYResolution(),
-      dimZ = i_pVolume->m_volumeMetaData.GetZResolution();
-  for (int i = 0; i < dimZ; i++) {
-    const VoxelType *p = i_pVolume->Slice(i);
-    fwrite(p, sizeof(VoxelType), dimX * dimY, fp);
-  }
-  fclose(fp);
-}
 
-void LoadVolume(std::vector<VoxelType>& volumedata) {
+void LoadVolume(std::vector<short>& volumedata) {
   FILE *fp = fopen("body.dat", "rb");
-  fread(&volumedata[0], sizeof(VoxelType), volumedata.size(), fp);
+  fread(&volumedata[0], sizeof(short), volumedata.size(), fp);
   fclose(fp);
 }
 
